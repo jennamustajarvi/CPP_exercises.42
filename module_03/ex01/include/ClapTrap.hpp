a29dcb57c1ac3b9fa5378b17dcc39d32f20ca816
Jenna Mustajärvi <jjen-_@hotmail.com>
2026-09-15 11:35:35 +0100

    begin mod03 ex01

--- module_03/ex01/include/ClapTrap.hpp
+++ module_03/ex01/include/ClapTrap.hpp
@@ -6,7 +6,33 @@
 /*   By: jemustaj <jemustaj@student.42Porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/14 22:35:08 by jemustaj          #+#    #+#             */
-/*   Updated: 2026/09/14 22:35:21 by jemustaj         ###   ########.fr       */
+/*   Updated: 2026/09/15 10:38:46 by jemustaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+
+class ClapTrap
+{
+	public:
+		ClapTrap(void);
+		ClapTrap(const std::string& name);
+		ClapTrap(const ClapTrap& other);
+		ClapTrap& operator=(const ClapTrap& other);
+		~ClapTrap(void);
+
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+
+	protected:
+		std::string		_name;
+		unsigned int	_hitPoints;
+		unsigned int	_energyPoints;
+		unsigned int	_attackDamage;
+};
+
+#endif
